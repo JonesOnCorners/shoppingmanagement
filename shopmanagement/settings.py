ef48daff7de7937cd1f5368444449b45a279cc5b
unknown <USER@SYNPUNLT1911068.int.asurion.com>
2020-06-22 18:17:23 +0530

    Changed settings file to allow hosts:

--- shopmanagement/settings.py
+++ shopmanagement/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=z&#748s!ri@5h13tpfyjjb3zn82z-2!^xvh*on^so%14u%c=d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
